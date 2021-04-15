36a069da2b26ac7ae5f4fac12032bcd2a1b8e5d4
mariu <s1095256@studenti.univpm.it>
2021-04-15 17:26:19 +0200

    Home e VistaHome.py

---
+++ home/views/VistaHome.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+
+
+
+class VistaHome(QWidget):
+    def __init__(self, parent=None):
+        super(VistaHome, self).__init__(parent)
