4137fcf744e98e72b792ab1cfdf49ec1f426c279
mariu <s1095256@studenti.univpm.it>
2021-04-16 16:35:41 +0200

    Impianto.py

---
+++ Impianti/Model/Impianto.py
@@ -0,0 +1,18 @@
+class Impianto():
+
+   # Costruttore con le informazioni di un impianto
+    def __init__(self, id, nome, tipo, posizione, prezzo):
+        super(Impianto, self).__init__()
+        self.id = id
+        self.nome = nome
+        self.posizione = posizione
+        self.tipo = tipo
+        self.prezzo = prezzo
+        self.disponibilità = True
+
+    # Funzione che ci indica se un impianto è disponibile o no
+    def is_disponibile(self):
+        return self.disponibilità
+    # Funzione che rende l'impiato prenotato non più disponibile
+    def prenota(self):
+        self.disponibilità = False
