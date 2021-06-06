24249ad23b9a555255f5655e1321dc2705f0ec9d
mariu <s1095256@studenti.univpm.it>
2021-06-06 15:52:00 +0200

    poiuyt

--- listaattrezzatura/views/VistaListaAttrezzature.py
+++ listaattrezzatura/views/VistaListaAttrezzature.py
@@ -28,7 +28,7 @@ class VistaListaAttrezzature(QWidget):
 
 
         buttons_layout = QVBoxLayout()
-        open_buttons = QPushButton("Visualizza informazioni")
+        open_buttons = QPushButton("Visualizza informazione")
         open_buttons.clicked.connect(self.show_selected_info)
         buttons_layout.addWidget(open_buttons)
         buttons_layout.addStretch()
