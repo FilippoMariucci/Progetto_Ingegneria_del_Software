204954570660ccb2596b41713c434cdae7bebff6
Mattia <s1093418@studenti.univpm.it>
2021-06-08 17:23:46 +0200

    implementate alcune funzioni

--- prenotazione/model/Prenotazione.py
+++ prenotazione/model/Prenotazione.py
@@ -1,10 +1,12 @@
 class Prenotazione():
-    def __init__(self, id, cliente, impianto, data, attrezzatura, totale):
+    def __init__(self, id, cliente, impianto, data, attrezzatura, orario, num_ore, totale):
         self.id = id
         self.cliente = cliente
         self.impianto = impianto
         self.data = data
         self.attrezzatura = attrezzatura
+        self.orario = orario
+        self.num_ore = num_ore
         self.totale = totale
 
 
